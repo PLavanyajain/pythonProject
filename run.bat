ede51b0d60c88c43d9fe6a53198afc63ab0e8f41
Lavanya <plavanya.896@gmail.com>
2020-12-01 20:01:35 +0530

    error is clear

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-rem pytest -s -v -m "sanity and regression" --html=Reports\reports.html Testcases--browser=chrome
-pytest -s -v -m "sanity or regression" --html=Reports\reports.html Testcases --browser=Ie
+pytest -s -v  --html=Reports\reports.html Testcases--browser=chrome
+rem pytest -s -v -m "sanity or regression" --html=Reports\reports.html Testcases --browser=Ie
 rem pytest -s -v -m "sanity "--html=Reports\reports.html Testcases --browser=chrome
 rem pytest -s -v -m "regression"--html=Reports\reports.html Testcases --browser=chrome
