07b90a4c93db018d71fecc3f39bce593330a7900
Lavanya <plavanya.896@gmail.com>
2020-12-02 14:45:24 +0530

    lavan

--- run.bat
+++ run.bat
@@ -1,4 +1,1 @@
-pytest -s -v -m "sanity and regression"  --html=Reports\reports.html Testcases --browser=chrome
-rem pytest -s -v -m "sanity or regression" --html=Reports\reports.html Testcases --browser=Ie
-rem pytest -s -v -m "sanity "--html=Reports\reports.html Testcases --browser=chrome
-rem pytest -s -v -m "regression"--html=Reports\reports.html Testcases --browser=chrome
+pytest -s -v --html=Reports\reports.html Testcases\Test_Addcustomer.py --browser=chrome
